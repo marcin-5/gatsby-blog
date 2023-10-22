0b221ab2a6bd3b4f922b5368bcb66be4298703d0
Marcin Bojara <marcin_5@mail.ru>
2023-10-22 15:13:20 +0200

    add templates and slugs for posts and pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,79 @@
 const path = require("path")
 
+// create slug for posts and pages
+const { createFilePath } = require("gatsby-source-filesystem")
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `content` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const content = await graphql(`
+    {
+      posts: allMarkdownRemark(
+        filter: { frontmatter: { type: { eq: "post" } } }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              published
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+      pages: allMarkdownRemark(
+        filter: { frontmatter: { type: { eq: "page" } } }
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  if (content.error) return
+  const allPosts = content.data.posts.edges
+  const allPages = content.data.pages.edges
+
+  // create the individual posts and pages
+  allPosts.forEach(({ node }) => {
+    if (node.frontmatter.published) {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve("./src/templates/Post.js"),
+        context: {
+          // data passed to context is available in page queries as graphql variables
+          slug: node.fields.slug,
+        },
+      })
+    }
+  })
+  allPages.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve("./src/templates/Page.js"),
+      context: {
+        // data passed to context is available in page queries as graphql variables
+        slug: node.fields.slug,
+      },
+    })
+  })
+}
+
 // Absolute imports
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
---
+++ src/components/Image/Image.styles.js
@@ -0,0 +1,9 @@
+import Img from "gatsby-image"
+import styled from "styled-components"
+
+export const Image = styled(Img)`
+  border-radius: 10px;
+  max-width: var(--site-max-width);
+  margin: 0 auto;
+  max-height: 300;
+`
